26e558268e74370eeaf035f8a680dfe36dde796f
Muchammad Rafli Riyana <56964502+mraflir@users.noreply.github.com>
2021-01-07 00:40:59 +0700

    styling main dan profile

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,191 +1,250 @@
-
 @extends('app')
+@section('css')
+<style>
+    .table .judulTabel {
+        border-top: none;
+        border-bottom: none;
+        color: #2F4159;
+        text-align: center;
+        font-weight: 400;
+    }
+
+    .container .cardTabel {
+        background: #F4DFCC;
+        border-radius: 15px;
+        max-width: 550px;
+        margin-bottom: 40px;
+        margin-top: 10px;
+    }
+
+
+    td {
+        color: #2F4159;
+        font-weight: bold;
+    }
+
+    @media (max-width: 992px) {
+        .container .cardTabel {
+            max-width: 450px;
+        }
+    }
+
+    @media (max-width: 768px) {
+        .container .cardTabel {
+            max-width: 370px;
+        }
+    }
+</style>
+@endsection
 @section('content')
 @csrf
-        <h1>Request from another player</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Nama</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody id="another-request">
-                
-            </tbody>
-        </table>
-        <h1>My own request</h1>
-          <table class="table">
-              <thead>
-                  <tr>
-                      <th>Nama</th>
-                  </tr>
-              </thead>
-              <tbody id="own-request">
-                  
-              </tbody>
-          </table>
-          <h1>My Match</h1>
-          <table class="table">
-              <thead>
-                  <tr>
-                      <th>Nama Lawan</th>
-                      <th>Aksi</th>
-                  </tr>
-              </thead>
-              <tbody id="my-match">
-                  
-              </tbody>
-          </table>
-          <h1>Online User</h1>
-          <table class="table">
-              <thead>
-                  <tr>
-                      <th>Nama</th>
-                      <th>Aksi</th>
-                  </tr>
-              </thead>
-              <tbody id="online-user">
-                  
-              </tbody>
-          </table>
-      
-          @endsection
-    @section('js')
-    <script>
-        function getOnlineUser(){
-            $.ajax({
-                'url':"{{url('/get-online-user')}}",
-                'method':'GET',
-                'success':function(data){
-                    let html=''
-                    data.forEach(element => {
-                        html+=`<tr><td>${element.name}</td><td><button type="button" class="btn btn-primary" onclick="sendRequest(${element.id})">Ajak bermain</button></td></tr>`
-                    });
-                    $('#online-user').html(html)
-                    setTimeout(() => {
-                        getOnlineUser()
-                    }, 500);
-                    
-                },
-                'error':function(){
-                    setTimeout(() => {
-                        getOnlineUser()
-                    }, 500);
-                }
-            })
-        }
+<div class="row justify-content-center">
 
-        function getAnotherRequest(){
-            $.ajax({
-                'url':"{{url('/get-another-request')}}",
-                'method':'GET',
-                'success':function(data){
-                    let html=''
-                    data.forEach(element => {
-                        html+=`<tr><td>${element.from.name}</td><td><button type="button" class="btn btn-primary" onclick="acceptRequest(${element.id})">Accept Request</button></td></tr>`
-                    });
-                    $('#another-request').html(html)
-                    setTimeout(() => {
-                        getAnotherRequest()
-                    }, 500);
-                    
-                },
-                'error':function(){
-                    setTimeout(() => {
-                        getAnotherRequest()
-                    }, 500);
-                }
-            })
-        }
-        function getOwnRequest(){
-            $.ajax({
-                'url':"{{url('/get-own-request')}}",
-                'method':'GET',
-                'success':function(data){
-                    let html=''
-                    data.forEach(element => {
-                        html+=`<tr><td>${element.to.name}</td></tr>`
-                    });
-                    $('#own-request').html(html)
-                    setTimeout(() => {
-                        getOwnRequest()
-                    }, 500);
-                    
-                },
-                'error':function(){
-                    setTimeout(() => {
-                        getOwnRequest()
-                    }, 500);
-                }
-            })
-        }
-        function getMatch(){
-            let my_id ="{{$my_id}}"
-            $.ajax({
-                'url':"{{url('/get-match')}}",
-                'method':'GET',
-                'success':function(data){
-                    let html=''
-                    data.forEach(element => {
-                        html+=`<tr><td>${element.first_player.id==my_id?element.second_player.name:element.first_player.name}</td><td><a href="{{url('game/')}}/${element.id}" type="button" class="btn btn-primary">Play</a></td></tr>`
-                    });
-                    $('#my-match').html(html)
-                    setTimeout(() => {
-                        getMatch()
-                    }, 500);
-                    
-                },
-                'error':function(){
-                    setTimeout(() => {
-                        getMatch()
-                    }, 500);
-                }
-            })
-        }
-        function sendRequest(id){
-            $.ajax({
-                    /* the route pointing to the post function */
-                    url: "{{url('/send-request')}}",
-                    type: 'POST',
-                    /* send the csrf-token and the input to the controller */
-                    data: {_token: $('input[name="_token"]').val(), id:id},
-                    dataType: 'JSON',
-                    /* remind that 'data' is the response of the AjaxController */
-                    success: function (data) { 
-                        console.log("success")
-                    },
-                    error:function(xhr, textStatus, errorThrown){
-                        console.log(errorThrown)
-                        console.log(xhr)
-                    }
-                }); 
-        }
+    <div class="col-xl-10 col-lg-12 col-md-10">
+        <div class="card o-hidden border-0 shadow-lg my-5 py-5" style="background-color: #4097AA; border-radius: 8px;">
+            <h1 class="text-center text-white">Request From Another Player</h1>
+            <div class="container cardTabel">
+                <table class="table">
+                    <thead>
+                        <tr class="row justify-content-around">
+                            <th class="col-5 text-left judulTabel">Nama</th>
+                            <th class="col-5 judulTabel">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody id="another-request">
 
-        function acceptRequest(id){
-            $.ajax({
-                    /* the route pointing to the post function */
-                    url: "{{url('/accept-request')}}",
-                    type: 'POST',
-                    /* send the csrf-token and the input to the controller */
-                    data: {_token: $('input[name="_token"]').val(), id:id},
-                    dataType: 'JSON',
-                    /* remind that 'data' is the response of the AjaxController */
-                    success: function (data) { 
-                        console.log("success")
-                    },
-                    error:function(xhr, textStatus, errorThrown){
-                        console.log(errorThrown)
-                        console.log(xhr)
-                    }
-                }); 
-        }
-        $(window).on('load',function(){
-            getOnlineUser()
-            getAnotherRequest()
-            getOwnRequest()
-            getMatch()
+                    </tbody>
+                </table>
+            </div>
+            <h1 class="text-center text-white">My Own Request</h1>
+            <div class="container cardTabel">
+                <table class="table">
+                    <thead>
+                        <tr class="row justify-content-around">
+                            <th class="col-11 text-left judulTabel">Nama</th>
+                        </tr>
+                    </thead>
+                    <tbody id="own-request">
+
+                    </tbody>
+                </table>
+            </div>
+            <h1 class="text-center text-white">My Match</h1>
+            <div class="container cardTabel">
+                <table class="table">
+                    <thead>
+                        <tr class="row justify-content-around">
+                            <th class="col-5 text-left judulTabel">Nama Lawan</th>
+                            <th class="col-5 judulTabel">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody id="my-match">
+
+                    </tbody>
+                </table>
+            </div>
+            <h1 class="text-center text-white">Online User</h1>
+            <div class="container cardTabel">
+                <table class="table">
+                    <thead>
+                        <tr class="row justify-content-around">
+                            <th class="col-5 text-left judulTabel">Nama</th>
+                            <th class="col-5 judulTabel">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody id="online-user">
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('js')
+<script>
+    function getOnlineUser() {
+        $.ajax({
+            'url': "{{url('/get-online-user')}}",
+            'method': 'GET',
+            'success': function(data) {
+                let html = ''
+                data.forEach(element => {
+                    html += `<tr><td>${element.name}</td><td><button type="button" class="btn btn-primary" onclick="sendRequest(${element.id})">Ajak bermain</button></td></tr>`
+                });
+                $('#online-user').html(html)
+                setTimeout(() => {
+                    getOnlineUser()
+                }, 500);
+
+            },
+            'error': function() {
+                setTimeout(() => {
+                    getOnlineUser()
+                }, 500);
+            }
         })
-    </script>
-  
-  @endsection
+    }
+
+    function getAnotherRequest() {
+        $.ajax({
+            'url': "{{url('/get-another-request')}}",
+            'method': 'GET',
+            'success': function(data) {
+                let html = ''
+                data.forEach(element => {
+                    html += `<tr><td>${element.from.name}</td><td><button type="button" class="btn btn-primary" onclick="acceptRequest(${element.id})">Accept Request</button></td></tr>`
+                });
+                $('#another-request').html(html)
+                setTimeout(() => {
+                    getAnotherRequest()
+                }, 500);
+
+            },
+            'error': function() {
+                setTimeout(() => {
+                    getAnotherRequest()
+                }, 500);
+            }
+        })
+    }
+
+    function getOwnRequest() {
+        $.ajax({
+            'url': "{{url('/get-own-request')}}",
+            'method': 'GET',
+            'success': function(data) {
+                let html = ''
+                data.forEach(element => {
+                    html += `<tr><td>${element.to.name}</td></tr>`
+                });
+                $('#own-request').html(html)
+                setTimeout(() => {
+                    getOwnRequest()
+                }, 500);
+
+            },
+            'error': function() {
+                setTimeout(() => {
+                    getOwnRequest()
+                }, 500);
+            }
+        })
+    }
+
+    function getMatch() {
+        let my_id = "{{$my_id}}"
+        $.ajax({
+            'url': "{{url('/get-match')}}",
+            'method': 'GET',
+            'success': function(data) {
+                let html = ''
+                data.forEach(element => {
+                    html += `<tr><td>${element.first_player.id==my_id?element.second_player.name:element.first_player.name}</td><td><a href="{{url('game/')}}/${element.id}" type="button" class="btn btn-primary">Play</a></td></tr>`
+                });
+                $('#my-match').html(html)
+                setTimeout(() => {
+                    getMatch()
+                }, 500);
+
+            },
+            'error': function() {
+                setTimeout(() => {
+                    getMatch()
+                }, 500);
+            }
+        })
+    }
+
+    function sendRequest(id) {
+        $.ajax({
+            /* the route pointing to the post function */
+            url: "{{url('/send-request')}}",
+            type: 'POST',
+            /* send the csrf-token and the input to the controller */
+            data: {
+                _token: $('input[name="_token"]').val(),
+                id: id
+            },
+            dataType: 'JSON',
+            /* remind that 'data' is the response of the AjaxController */
+            success: function(data) {
+                console.log("success")
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                console.log(errorThrown)
+                console.log(xhr)
+            }
+        });
+    }
+
+    function acceptRequest(id) {
+        $.ajax({
+            /* the route pointing to the post function */
+            url: "{{url('/accept-request')}}",
+            type: 'POST',
+            /* send the csrf-token and the input to the controller */
+            data: {
+                _token: $('input[name="_token"]').val(),
+                id: id
+            },
+            dataType: 'JSON',
+            /* remind that 'data' is the response of the AjaxController */
+            success: function(data) {
+                console.log("success")
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                console.log(errorThrown)
+                console.log(xhr)
+            }
+        });
+    }
+    $(window).on('load', function() {
+        getOnlineUser()
+        getAnotherRequest()
+        getOwnRequest()
+        getMatch()
+    })
+</script>
+
+@endsection
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,17 +1,65 @@
 @extends('app')
+@section('css')
+<style>
+    .cardStatistik {
+        background: #ECF3FB;
+        color: #2F4159;
+        border-radius: 15px;
+        margin-bottom: 40px;
+        margin-top: 10px;
+    }
+
+    .cardStatistik h1 {
+        font-size: 30px
+    }
+
+    .stat {
+        background: #ECF3FB;
+        color: #2F4159;
+        border-radius: 15px;
+        margin-top: 10px;
+    }
+
+    .stat h1 {
+        font-size: 20px
+    }
+</style>
+@endsection
 @section('content')
-    @foreach($users as $user)
-        <img src="http://placecorgi.com/250">
-        <h1>Nama:{{$user->name}}</h1>
-        <h1>Email:{{$user->email}}</h1>
-        <h1>Menang:{{$win}}</h1>
-        <h1>Kalah:{{$lose}}</h1>
-        <h1>Draw:{{$draw}}</h1>
-        <h1>Jumlah Main:{{$win+$lose+$draw}}</h1>
-        @if($win+$lose+$draw)
-        <h1>Rata-rata menang:{{$win*100/($win+$lose+$draw)}}%</h1>
-        <h1>Rata-rata kalah:{{$lose*100/($win+$lose+$draw)}}%</h1>
-        <h1>Rata-rata seri:{{$draw*100/($win+$lose+$draw)}}%</h1>
-        @endif
-    @endforeach
+@foreach($users as $user)
+<div class="row justify-content-center">
+    <div class="col-xl-10 col-lg-12 col-md-9">
+        <div class="card o-hidden border-0 shadow-lg my-5 py-5 px-5" style="background-color: #4097AA; border-radius: 8px;">
+            <h1 class="text-center text-white mb-3" style="font-size: 60px;">Profile</h1>
+            <div class="row justify-content-center">
+                <div class="col-10 col-lg-4 text-center mb-3">
+                    <img src="http://placecorgi.com/250" class="image-fluid" style="border-radius: 50%;">
+                </div>
+                <div class="w-100 d-block d-lg-none"></div>
+                <div class="col-10 col-lg-6 align-self-center text-center text-lg-left text-white mb-3">
+                    <h1 style="font-size: 35px;">Nama: {{$user->name}}</h1>
+                    <h1 style="font-size: 35px;">Email: {{$user->email}}</h1>
+                    <h1 style="font-size: 35px;">Jumlah Main: {{$win+$lose+$draw}}</h1>
+                    <div class="stat px-3 mt-3 pt-1">
+                        <div class="row justify-content-center text-center">
+                            <h1 class="col-10 col-lg-4">Menang: {{$win}}</h1>
+                            <h1 class="col-10 col-lg-4">Kalah: {{$lose}}</h1>
+                            <h1 class="col-10 col-lg-4">Draw: {{$draw}}</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="cardStatistik">
+                <div class="row justify-content-center text-center">
+                    @if($win+$lose+$draw)
+                    <h1 class="col-10 col-4">Rata-rata menang: {{$win*100/($win+$lose+$draw)}}%</h1>
+                    <h1 class="col-10 col-4">Rata-rata kalah: {{$lose*100/($win+$lose+$draw)}}%</h1>
+                    <h1 class="col-10 col-4">Rata-rata seri: {{$draw*100/($win+$lose+$draw)}}%</h1>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
 @endsection
