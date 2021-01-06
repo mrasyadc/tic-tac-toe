4b342da1834b69ccf4afb85c81fa1818af0e9435
Muchammad Rafli Riyana <56964502+mraflir@users.noreply.github.com>
2021-01-07 02:32:14 +0700

    styling edit + navbar dikit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,45 +1,47 @@
 <!doctype html>
 <html lang="en">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    @yield('css')
-  </head>
-  <body style="background-color: #2F4159;">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+@yield('css')
+</head>
+
+<body style="background-color: #2F4159;">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="color: #2F4159;">
         <div class="container">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
-          <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Navbar</a>
 
-          <div class="collapse navbar-collapse" id="navbarToggler">
-              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                  <li class="nav-item">
-                      <a class="nav-link" href="/">Game</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="/profile">Profile</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="/edit">Account Setting</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="/logout">Logout</a>
-                  </li>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Game</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/profile">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/edit">Account Setting</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </li>
 
-              </ul>
-          </div>
-      </div>
-    </nav>
-      <div class="container">
-          @yield('content')
+                </ul>
+            </div>
         </div>
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        @yield('js')
-        </body>
-        </html>
+    </nav>
+    <div class="container">
+        @yield('content')
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    @yield('js')
+</body>
+
+</html>
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,44 +1,105 @@
 @extends('app')
+@section('css')
+<style>
+    .tulisanEdit {
+        color: #2F4159;
+        font-weight: bold;
+    }
+
+    .tombol {
+        max-width: 380px;
+        display: inline;
+    }
+
+    .tombolBatal {
+        max-width: 380px;
+        background-color: #FFC3B8;
+        border: #FFC3B8;
+        display: inline;
+        color: #2F4159;
+    }
+
+    .tombolBatal:hover {
+        background-color: #E3A89E;
+    }
+</style>
+@endsection
 @section('content')
-{{--    @foreach($user as $u)--}}
-{{--        <h1>Foto:{{$u->id}}</h1>--}}
-{{--        <h1>Password:{{$u->email}}</h1>--}}
-{{--    @endforeach--}}
+{{-- @foreach($user as $u)--}}
+{{-- <h1>Foto:{{$u->id}}</h1>--}}
+{{-- <h1>Password:{{$u->email}}</h1>--}}
+{{-- @endforeach--}}
 @foreach($user as $u)
-    <form action="/edit/proses" method="post">
-        {{ csrf_field() }}
-        <input type="hidden" name="id" value="{{ $u->id }}"> <br/>
-        Nama <input type="text" required="required" name="name" value="{{ $u->name }}" /> <br/>
-        Password Lama <input type="password" required="required" name="oldPassword" /> <br/>
-        Password Baru <input id="newPassword" type="password" required="required" name="newPassword" onkeyup="check()"/> <br/>
-        Retype Password Baru <input id="newPasswordRetyped" type="password" required="required" name="newPasswordRetyped" onkeyup="check()"/> <br/>
-        <input type="submit" value="Simpan Data">
-        <a href=".."><button>Batalkan</button></a>
-    </form>
-    <div id="alert"></div>
-    @if($errors->any())
-        <div class="alert alert-danger">{{$errors->first()}}</div>
-    @endif
-    <br/>
-    <form action="/edit/gambar" method="post" enctype="multipart/form-data">
-        {{ csrf_field() }}
-        <input type="file" name="file"/><br/>
-        <input type="submit" value="Upload"/>
-    </form>
-    <a href="/profile"><button>Batalkan</button></a>
+<!-- Ganti password -->
+<div class="row justify-content-center">
+
+    <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5" style="background-color: #4097AA; border-radius: 8px;">
+            <div class="row">
+                <div class="col">
+                    <div class="p-5">
+                        <h1 class="text-center text-white mb-3" style="font-size: 60px;">Edit Your Account</h1>
+                        <form action="/edit/proses" method="post">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="id" value="{{ $u->id }}">
+                            <br />
+                            <div class="form-group tulisanEdit">
+                                Nama <input class="form-control form-control-user" type="text" required="required" name="name" value="{{ $u->name }}" />
+                            </div>
+                            <br />
+                            <div class="form-group tulisanEdit">
+                                Password Lama <input class="form-control form-control-user" type="password" required="required" name="oldPassword" placeholder="Masukkan Password lama..." />
+                            </div>
+                            <br />
+                            <div class="form-group tulisanEdit">
+                                Password Baru <input class="form-control form-control-user" id="newPassword" type="password" required="required" name="newPassword" placeholder="Masukkan Password baru..." onkeyup="check()" />
+                            </div>
+                            <br />
+                            <div class="form-group tulisanEdit">
+                                Retype Password Baru <input class="form-control form-control-user" id="newPasswordRetyped" type="password" required="required" name="newPasswordRetyped" placeholder="Retype Password baru..." onkeyup="check()" />
+                            </div>
+                            <br />
+                            <div class="text-center mb-3">
+                                <input class="btn btn-primary btn-user btn-block tombol" type="submit" value="Simpan Data">
+                                <a href=".."><button class="btn btn-primary btn-user btn-block tombolBatal">Batalkan</button></a>
+                            </div>
+                        </form>
+                        <div id="alert"></div>
+                        @if($errors->any())
+                        <div class="alert alert-danger">{{$errors->first()}}</div>
+                        @endif
+
+                        <!-- Ganti Poto -->
+                        <br />
+                        <h1 class="text-center text-white mb-3 mt-3" style="font-size: 60px;">Edit Profile Photo</h1>
+                        <form action="/edit/gambar" method="post" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <input type="file" name="file" /><br />
+                            <div class="text-center my-3">
+                                <input class="btn btn-primary btn-user btn-block tombol" type="submit" value="Upload" />
+                        </form>
+                        <a href="/profile"><button class="btn btn-primary btn-user btn-block tombolBatal">Batalkan</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 @endforeach
 @endsection
 @section('js')
-    <script>
+<script>
     var check = function() {
         if (document.getElementById('newPassword').value ===
             document.getElementById('newPasswordRetyped').value) {
-                document.getElementById('alert').className = 'alert alert-success';
-                document.getElementById('alert').innerHTML = 'Your Password Match';
+            document.getElementById('alert').className = 'alert alert-success';
+            document.getElementById('alert').innerHTML = 'Your Password Match';
         } else {
             document.getElementById('alert').className = 'alert alert-danger';
             document.getElementById('alert').innerHTML = 'Your Password Not Match';
         }
     }
-    </script>
+</script>
 @endsection
